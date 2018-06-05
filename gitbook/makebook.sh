098b80eadecad6e3138c08c99228c4783be636eb
liuguang <67579722@qq.com>
2018-06-05 11:00:23 +0800

    生成pdf时显示debug信息

--- gitbook/makebook.sh
+++ gitbook/makebook.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
+show_error(){
+        echo -e "\033[41;36m $1 \033[0m"
+}
+
 if [ "$#" -ge 2 ]; then
-	cd $2
-	gitbook $1 .
+        cd $2
+        gitbook $1 . --log debug
 else
-	echo "param error">>/dev/stderr
+        show_error "input param error"
 fi
